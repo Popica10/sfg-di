4af45220999afad3c055285eeedaf5b84652f172
Popika <andreipop.acs@gmail.com>
2023-01-22 00:41:11 +0200

    sfg-di - Spring Context & DI & IoC

--- src/main/java/com/example/sfgdi/SfgDiApplication.java
+++ src/main/java/com/example/sfgdi/SfgDiApplication.java
@@ -1,13 +1,17 @@
 package com.example.sfgdi;
 
+import com.example.sfgdi.controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SfgDiApplication.class, args);
+        ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+        MyController controller = (MyController) context.getBean("myController");
+        System.out.println(controller.helloWorld());
     }
 
 }
--- src/main/java/com/example/sfgdi/controller/MyController.java
+++ src/main/java/com/example/sfgdi/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.example.controller;
+package com.example.sfgdi.controller;
 
 import org.springframework.stereotype.Controller;
 
